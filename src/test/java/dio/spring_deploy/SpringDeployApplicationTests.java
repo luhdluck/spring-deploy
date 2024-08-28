beac33f4d3a8526925c6278fc20bfbb1a1bba13f
Luisa Lemos <luisa_dluck@hotmail.com>
2024-08-28 20:50:15 -0300

    Initial commit

---
+++ src/test/java/dio/spring_deploy/SpringDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.spring_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
